7b83961fb2195f7f3c07e5ee5b948e3cb9fbafcb
Rahul Singh Shekhawat <rahulsinghshekhawat16@gmail.com>
2022-05-15 16:00:41 +0530

    auth config file added

---
+++ config/auth.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    secret: "task-manager-secret-key",
+    user: "youraccount@gmail.com",
+    pass: "youraccountpassworf",
+};
+
+// Visit to https://myaccount.google.com/apppasswords and create app and use password here to send email by gmail account
