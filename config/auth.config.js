acdb20b5ed6667add8fee3ee3e364a6300d663a1
rahul singh shekhawat <rahulsinghshekhawat16@gmail.com>
2022-05-17 11:08:02 +0530

    Untrack files in .gitignore

--- config/auth.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    secret: "task-manager-secret-key",
-    user: "youraccount@gmail.com",
-    pass: "youraccountpassworf",
-};
-
-// Visit to https://myaccount.google.com/apppasswords and create app and use password here to send email by gmail account
